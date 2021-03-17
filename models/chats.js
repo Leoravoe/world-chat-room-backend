fac0294bf868aa1757be64e412851ebf873fbddd
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-03-17 20:59:38 +0530

    models changed in model/chats

--- models/chats.js
+++ models/chats.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const chats = mongoose.Schema({
     name:{type:String},
     chats : {type: String},
+    received : {type: Boolean},
 })
 
 module.exports = mongoose.model('chats',chats);
