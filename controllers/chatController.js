01a498b2da18311c2e195fe6e28a728eecef0e2f
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-03-17 20:50:31 +0530

    nothing much

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,8 +4,8 @@ const Chat = require('../models/chats');
 module.exports.chat = async (req,res) => {
     try {
         const chat = await Chat.create(req.body);
-        // console.log(chat)
-        res.status(201).send(chat)
+        console.log(chat)
+        res.status(201).json(chat)
     } catch (error) {
         res.status(404).json(error);
     }
